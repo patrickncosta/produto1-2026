8aa6174e9458a1ac07728d67babbe9d3607082e6
patrickncosta <patrick.nunes.diplomata@gmail.com>
2026-05-08 11:39:44 -0300

    19 - avançando com o spring security

---
+++ src/main/java/br/ifmg/produto1_2026/resources/exception/ValidationError.java
@@ -0,0 +1,17 @@
+package br.ifmg.produto1_2026.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandartError{
+
+    private List<FieldMessage> fieldMessages = new ArrayList<FieldMessage>();
+
+    public List<FieldMessage> getFieldMessages() {
+        return fieldMessages;
+    }
+
+    public void addFieldMessage(FieldMessage fieldMessage){
+        fieldMessages.add(fieldMessage);
+    }
+}
